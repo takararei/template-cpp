4db2bb36b8136d03df617d50155cd0546423dfcb
takararei <327967531@qq.com>
2021-11-26 16:25:31 +0000

    simple project

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspace/template-cpp
-# Build directory: /workspace/template-cpp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
-subdirs("src")
